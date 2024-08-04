79b82ac2c9f3e8f49de7ad8b19639e65274cb34f
vinay-3333 <vagrawal032@gmail.com>
2024-08-04 22:39:09 +0530

    deploy application

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
+# load_dotenv()
+# os.getenv("GOOGLE_API_KEY")
+API_Token=st.secrets['GOOGLE_API_KEY']
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
